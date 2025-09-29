aa706a3ba8dd1f3e652c9e04c65850682df46429
himanshu kumar <writetohimanshukumar@gmail.com>
2025-09-30 01:27:01 +0530

    add user to database after redirect from auth0

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,20 @@ import './index.css';
 import { BrowserRouter } from 'react-router';
 import AppRoutes from './AppRoutes';
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
-            <Auth0ProviderWithNavigate>
-                <AppRoutes />
-            </Auth0ProviderWithNavigate>
+            <QueryClientProvider client={queryClient}>
+                <Auth0ProviderWithNavigate>
+                    <AppRoutes />
+                </Auth0ProviderWithNavigate>
+            </QueryClientProvider>
         </BrowserRouter>
     </StrictMode>
 );
