93986c4a5483a0081ac43fe377d76171fbd9de63
himanshu kumar <writetohimanshukumar@gmail.com>
2025-09-29 20:12:43 +0530

    auth0 setup

--- frontend/src/components/DesktopNav.tsx
+++ frontend/src/components/DesktopNav.tsx
@@ -1,8 +1,16 @@
 import { Button } from './ui/button';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const DesktopNav = () => {
+    //TODO: write a common button for a single source of signin functionality
+    const { loginWithRedirect } = useAuth0();
+
     return (
-        <Button variant="ghost" className="font-bold">
+        <Button
+            variant="ghost"
+            className="font-bold"
+            onClick={async () => await loginWithRedirect()}
+        >
             Login
         </Button>
     );
