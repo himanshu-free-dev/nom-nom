11106d486f7265abcd5e956ede1c5d2d90aefab3
himanshu kumar <writetohimanshukumar@gmail.com>
2025-10-01 00:35:06 +0530

    update user profile backend

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,7 +1,52 @@
+import type { NextFunction, Request, Response } from 'express';
 import { auth } from 'express-oauth2-jwt-bearer';
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js';
+
+declare global {
+    namespace Express {
+        interface Request {
+            userId: string;
+            auth0Id: string;
+        }
+    }
+}
 
 export const jwtCheck = auth({
     audience: process.env.AUTH0_AUDIENCE as string,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL as string,
     tokenSigningAlg: 'RS256',
 });
+
+export const jwtParse = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization?.startsWith('Bearer ')) {
+        return res.sendStatus(401);
+    }
+
+    const token = authorization.split(' ')[1];
+
+    try {
+        const decoded = jwt.decode(token as string);
+        const auth0Id = decoded?.sub;
+
+        const user = await User.findOne({ auth0Id });
+
+        if (!user) {
+            return res.sendStatus(401);
+        }
+
+        req.auth0Id = auth0Id as string;
+        req.userId = user._id.toString();
+
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(401);
+    }
+};
